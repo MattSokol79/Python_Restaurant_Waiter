0fa99b3d9c69dec0901f5969f006512c25aa3da7
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-05 16:03:15 +0000

    Added main functions to run program

--- waiter_helper.py
+++ waiter_helper.py
@@ -7,6 +7,10 @@ class Waiter(Menu):
         super().__init__() # Inherits the menu from the menu class
         self.order_list = []
 
+    # This is the ordering method, every time a person is asked
+    # what they want ordered, they can input an item on the meny
+    # if the item does not exist on the menu, they will be told thats the case
+
     def order(self):
         ordering = True
 
@@ -23,6 +27,18 @@ class Waiter(Menu):
 
         return self.order_list
 
+    # This __str__ allows us to display what was ordered once the program
+    # Has run its course
     def __str__(self):
         return "You ordered {}".format(self.order_list)
 
+def main():
+    customer = Waiter()
+
+    print("Welcome to the restaurant.")
+    customer.order()
+    print(f"You have ordered: {customer}")
+
+# Checks if the code is running from the current file
+if __name__ == "__main__":
+    main()
