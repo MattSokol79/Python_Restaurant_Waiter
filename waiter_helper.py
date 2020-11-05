72290bb0fc192de6bc5627fa351f8be5cf1638db
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-05 19:42:21 +0000

    Waiter Program Complete first iteration

--- waiter_helper.py
+++ waiter_helper.py
@@ -5,41 +5,40 @@ from menu import Menu
 class Waiter(Menu):
     def __init__(self):
         super().__init__() # Inherits the menu from the menu class
-        self.order_list = [] # Creates a list for the class
+        self.order_list = [] # Creates an empty order list
 
-    # This is the ordering method, every time a person is asked
-    # what they want ordered, they can input an item on the meny
-    # if the item does not exist on the menu, they will be told thats the case
-
+    # This is the ordering method, order number starts at 0, and increments up to 3
     def order(self):
-        ordering = True
-        print("What would you like? ")
-
-        while ordering:
-            choice = input("---> ")
-
-            # Item must be available in the menu to be ordered
-            if choice in self.menu:
-                self.order_list.append(choice)
+        order_number = 0
+        # A diner is greeted with the menu
+        print("This is the current menu, due to company policy, "
+              "diners may only order maximum 3 items", self.menu)
+        print("What would you like to order?")
+        while order_number < 3: # We only want the diner to order 3 items
+            diner_order = input("-->")
+            # If the specified item is present in the menu, we can add it to the order_list
+            if diner_order in self.menu:
+                self.order_list.append(diner_order)
+                order_number += 1
             else:
-                print("I'm sorry, we do not serve that here")
-
-            if choice.lower() == "no":
-                ordering = False
-
-        return self.order_list
+                print("Sorry, we are currently not serving this item")
+        # If we have reached more than 3 items, we return the order to the diner
+        else:
+            return self.order_list
 
     # This __str__ allows us to display what was ordered once the program
     # Has run its course
     def __str__(self):
-        return "You ordered {}".format(self.order_list)
+        return "{}".format(self.order_list)
 
+# Main allows us to run the code from this file, so no need to import
+# the classes into another .py file
 def main():
     customer = Waiter()
 
     print("Welcome to the restaurant.")
     customer.order()
-    print(f"You have ordered: {customer}")
+    print(f"You have ordered:\n {customer}")
 
 # Checks if the code is running from the current file
 if __name__ == "__main__":
